45aff5df4b27ef183345bb233ebd5bb4b71931f6
iggy <bcus9126@gmail.com>
2026-03-02 04:00:19 -0600

    added full_headless_script

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # testing-scripts
 Shell scripts for reproducibly testing parts of the rover through ROS
 
-The easiest way to run these scripts on the rover is to enter the `rover-ros2` directory.
+The easiest way to run these scripts on the rover is to enter the `rover-ros2` directory before running them with `~/testing-scripts/<your-script>`
 
+The full_headless script needs to be run in a kitty window with remote-control enabled. Run `kitty -o allow_remote_control=yes` to open one  
---
+++ full_headless.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+trap abort INT
+
+function abort() {
+    # kitten @ close-tab --match title:"Fake Serial|Anchor|Headless|Serial Viewer" #fix later
+    kitten @ close-tab --match "not state:focused and state:parent_focused" #hacky workaround because youll probably be focusing the window if you kill it
+    echo 
+    exit 0
+}
+
+kitten @ launch --type=tab --tab-title "Fake Serial" socat -dd -v pty,rawer,crnl,link=/tmp/ttyACM9 pty,rawer,crnl,link=/tmp/ttyOUT
+kitten @ launch --type=tab --tab-title "Serial Viewer" tio /tmp/ttyACM9
+
+kitten @ launch --type=tab --cwd current --tab-title "Anchor" 
+kitten @ launch --type=tab --cwd current --tab-title "Headless" 
+
+kitty @ send-text --match-tab title:"Anchor|Headless" source install/setup.bash'\n'
+kitty @ send-text --match-tab title:"Anchor|Headless" colcon build --symlink-install'\n'
+
+kitty @ send-text --match-tab title:Anchor PORT_OVERRIDE=/tmp/ttyACM9 ros2 launch anchor_pkg rover.launch.py'\n'
+kitty @ send-text --match-tab title:Headless ros2 run headless_pkg headless_full'\n'
+
+read -r -d '' _ </dev/tty #keep script open
+
